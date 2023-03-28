3da953048368d4969c96285b4887b85a9e0c16e6
unknown <dhruv.uberoi@gmail.com>
2023-03-28 21:33:32 +0530

    needs some changes

---
+++ server/routes/users.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { getUser, getUserFriends, addRemoveFriend } from "../controllers/users.js"
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+// read
+
+router.get("/:id", verifyToken, getUser);
+router.get("/:id", verifyToken, getUserFriends);
+
+// update
+
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+export default router;
